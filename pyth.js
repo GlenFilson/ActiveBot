d87d337c94f025008f173485965ae227275ac626
GlenFilson <glenfilson@outlook.com>
2025-10-22 15:06:33 +0100

    Name change and gitignore

--- pyth.js
+++ pyth.js
@@ -1,7 +1,6 @@
 import WebSocket from "ws";
 
-const COIN = "SOL";
-const MINT = "So11111111111111111111111111111111111111112";
+const ASSET = "SOL";
 const PYTH_ID =
   "ef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d";
 
@@ -11,19 +10,17 @@ const ws = new WebSocket(WS_URL); //creates a new websocket connection to the py
 //when the websocket connection is opened
 ws.onopen = () => {
   console.log("Connected to Pyth Websocket");
-  //when the connection is opened, subscribe to price updates for the coin
-  console.log(`Subscribing to ${COIN} price updates...`);
+  //when the connection is opened, subscribe to price updates for the asset
+  console.log(`Subscribing to ${ASSET} price updates...`);
   ws.send(
     JSON.stringify({
       type: "subscribe",
       ids: [PYTH_ID],
     })
   );
-  console.log(`Subscribed to ${COIN} price updates`);
+  console.log(`Subscribed to ${ASSET} price updates`);
 };
 
 ws.onmessage = (event) => {
   const data = JSON.parse(event.data);
-  console.log(data);
-  
 };
